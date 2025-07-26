32d35d1ae9638784e834475a97712c16852c1560
630masarouta <gaoliuyaxing@gmail.com>
2025-07-26 23:20:24 +0900

    Issue4

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_26_123628) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_26_140353) do
   create_table "plans", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "plan"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
